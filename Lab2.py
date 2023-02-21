836c2840454b95b91445af28b24663ceb638c91d
Justin Hill <jhill27@myseneca.ca>
2023-02-20 23:51:06 -0500

    Updates Lab2.py

--- Lab2.py
+++ Lab2.py
@@ -3,3 +3,10 @@ import sys
 def variableCheck(Variable1, Variable2):
     print("The first command line argument is: "+ Variable1)
     print("The second command line argument is: "+ Variable2)
+
+
+def helloWorld():
+	print("Hello World")
+
+
+helloWorld()
